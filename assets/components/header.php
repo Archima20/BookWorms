96ccabfb92a13e7af66da8f8b9df561d73afa108
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-01-26 22:51:03 +0100

    new branck no tailwind instalation but link to

---
+++ assets/components/header.php
@@ -0,0 +1,14 @@
+<head>
+<link rel="stylesheet" href="./styles/main.css">
+</head>
+<body>
+<nav class="box-nav">
+    <img class="img-logo" src="./assets/images/Logobworms.png" alt="">
+      <form class="frm-srch" role="search">
+        <input class="inpt-srch" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn-srch" type="submit">
+          <img src="../images/lupa.png" alt="">
+        </button>
+      </form>
+    </nav>
+</body>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!--<?php include "calldb.php" ?>-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Bookworms library</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/main.css">
+  </head>
+<body>
+  <?php include "./assets/components/header.php" ?>
+  <?php include "./assets/components/books.php" ?>
+ 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+</html>
