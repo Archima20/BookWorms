8f4fb8b7f7b5651787d77ad40e432cff1dda09fa
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-01-27 12:55:11 +0100

    create form incorporated

---
+++ assets/components/create.php
@@ -0,0 +1,42 @@
+<head>
+    <link rel="stylesheet" href="./styles/main.css">
+</head>
+<body>
+<form action="#" method="POST">
+        <div class="overflow-hidden shadow sm:rounded-md">
+          <div class="bg-white px-4 py-5 sm:p-6">
+            <div class="grid grid-cols-6 gap-6">
+
+              <div class="col-span-6 sm:col-span-3">
+                <input type="text" name="title" autofocus="autofocus" require="require" placeholder="Title" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" >
+              </div>
+
+              <div class="col-span-6 sm:col-span-4">
+                <input type="text" name="authors" require="require" placeholder="Author" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+              </div>
+
+              <div class="col-span-6">
+                <input type="text" name="isbn" require="require" placeholder="ISBN" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+              </div>
+
+              <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                <input type="text" name="imageUrl"  require="require" placeholder="Image URL"  class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+              </div>
+
+              <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                <textarea rows="4" cols="1" type="text" name="description" require="require" placeholder="Description" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"></textarea> 
+
+              </div>
+            </div>
+          </div>
+          <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+          </div>
+
+          <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <a href="index.php" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Cancel</a>
+          </div>
+
+        </div>
+      </form>
+</body>
--- index.php
+++ index.php
@@ -15,6 +15,7 @@
   </head>
 <body>
   <?php include "./assets/components/header.php" ?>
+  <?php include "./assets/components/create.php" ?>
   <?php include "./assets/components/books.php" ?>
  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
