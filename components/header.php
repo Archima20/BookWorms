ddc76b73198387d7343b516efb66d5d9ac461b91
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-01-30 15:44:57 +0100

    cambios iniciales en header tailwinds

--- components/header.php
+++ components/header.php
@@ -1,14 +1,13 @@
-<head>
-<link rel="stylesheet" href="./styles/main.css">
-</head>
-<body>
-<nav class="box-nav">
-    <a href="/BookWorms"><img class="img-logo" src="./assets/images/Logobworms.png" alt="Logo"></a>
-      <form class="frm-srch box-search" method="GET" action="">
-        <input class="inpt-srch input-search" type="search" autocomplete="off" placeholder="Search books by title / author" aria-label="Search" label="Search" name="keyword" value="<?php echo isset($_GET['keyword']) ? $_GET['keyword'] : '' ?>">
-        <button class="btn-srch" type="submit" name="search">
-          <img class="img-search" src="./assets/images/lupa.png" alt="Magnifier glass">
-        </button>
-      </form>
-    </nav>
-</body>
+<nav class="box-nav p-5">
+  <div class="flex justify-between items-center">
+     <a href="/BookWorms"><img src="./assets/images/Logobworms.png" alt="Logo"></a>
+    <a href="/BookWorms"><img src="./assets/images/casita.png" alt="Logo"></a>
+  </div>
+   
+  <form class="flex justify-between items-center rounded-md" method="GET">
+    <input class="input-search" type="search" autocomplete="off" placeholder="Search books by title / author" aria-label="Search" label="Search" name="keyword" value="<?php echo isset($_GET['keyword']) ? $_GET['keyword'] : '' ?>">
+    <button class="btn-srch" type="submit" name="search">
+      <img class="img-search" src="./assets/images/lupa.png" alt="Magnifier glass">
+    </button>
+  </form>
+</nav>
