f44fb1c7e3f2dd166239f2611fc6108120d44847
pohengchan <pohengchan@gmail.com>
2023-01-30 16:19:29 +0100

    display chosen book

--- components/books.php
+++ components/books.php
@@ -22,8 +22,9 @@
             while($row = $result->fetch()){
                 echo "<div href='#' class='group'>";
                     echo "<div class='aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8'>
+                    <a href='./displaypage.php?id=". $row['ID'] ."' class='mr-3' title='View Book' data-toggle='tooltip'>
                     <img src='" . $row['book_image'] . "' alt='" . $row['title'] . "' class='h-full w-full object-cover object-center group-hover:opacity-75'>
-                    </div>";
+                    </a></div>";
                     echo "<h3 class='mt-4 text-sm text-gray-700'>Title</h3>";
                     echo "<p>" . $row['title'] . "</p>";
                     echo "<p class='mt-1 text-lg font-medium text-gray-900'>Author</p>";
