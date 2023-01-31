e169f8035e0f07ad3115f67941abcf3058fdfd1b
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-01-31 10:42:17 +0100

    create test

--- components/create.php
+++ components/create.php
@@ -1,3 +1,94 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$ID = $title = $author = $ISBN = $decription = $book_image ="";
+$ID_err = $title_err = $author_err = $ISBN_err = $decription_err = $book_image_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate title
+    $input_title = trim($_POST["title"]);
+    if(empty($input_title)){
+        $title_err = "Please enter a title for the book.";     
+    } else{
+        $title = $input_title;
+    }
+
+    // Validate author
+    $input_author = trim($_POST["author"]);
+    if(empty($input_author)){
+        $author_err = "Please enter an author for the book.";
+    } elseif(!filter_var($input_author, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+        $author_err = "Please enter a valid name for the author.";
+    } else{
+        $author = $input_author;
+    }
+    
+    // Validate ISBN
+    $input_ISBN = trim($_POST["ISBN"]);
+    if(empty($input_ISBN)){
+        $ISBN_err = "Please enter an ISBN.";     
+    } else{
+        $ISBN = $input_ISBN;
+    }
+    
+    // Validate description
+    $input_description = trim($_POST["description"]);
+    if(empty($input_description)){
+        $description_err = "Please enter a description.";     
+    } else{
+        $description = $input_description;
+    }
+
+    // Validate book image
+    $input_book_image = trim($_POST["book_image"]);
+    if(empty($input_book_image)){
+        $book_image_err = "Please enter an URL for the image.";     
+    } else{
+        $book_image = $input_book_image;
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($title_err) && empty($author_err) && empty($ISBN_err) && empty($description_err) && empty($book_image_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO books (title, author, ISBN, description, book_image) VALUES (:title, :author, :ISBN, :description, :book_image)";
+ 
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":title", $param_title);
+            $stmt->bindParam(":author", $param_author);
+            $stmt->bindParam(":ISBN", $param_ISBN);
+            $stmt->bindParam(":description", $param_description);
+            $stmt->bindParam(":book_image", $param_book_image);
+            
+            // Set parameters
+            $param_title = $title;
+            $param_author = $author;
+            $param_ISBN = $ISBN;
+            $param_description = $description;
+            $param_book_image = $book_image;
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Records created successfully. Redirect to landing page
+                header("location: index.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        unset($stmt);
+    }
+    
+    // Close connection
+    unset($pdo);
+}
+?>
+
 <div class="w-full flex flex-col items-center">
   <div class="my-6 w-5/6">
     <div class="flex mb-1.5">
@@ -10,9 +101,9 @@
   <form action="#" method="POST" class = "mt-2 w-5/6">
       <div class="grid gap-5">
           <input type="text" name="title" autofocus="autofocus" require="require" placeholder="Title" class="border border-solid border-blue-600 bg-yellow-50 rounded-md placeholder:text-blue-400 placeholder:text-sm placeholder:font-bold placeholder:font-sans p-2 w-full" >
-          <input type="text" name="authors" require="require" placeholder="Author" class="border border-solid border-blue-600 bg-yellow-50 rounded-md placeholder:text-blue-400 placeholder:text-sm placeholder:font-bold placeholder:font-sans p-2 w-full">
-          <input type="text" name="isbn" require="require" placeholder="ISBN" class="border border-solid border-blue-600 bg-yellow-50 rounded-md placeholder:text-blue-400 placeholder:text-sm placeholder:font-bold placeholder:font-sans p-2 w-full">
-          <input type="text" name="imageUrl"  require="require" placeholder="Image URL"  class="border border-solid border-blue-600 bg-yellow-50 rounded-md placeholder:text-blue-400 placeholder:text-sm placeholder:font-bold placeholder:font-sans p-2 w-full">
+          <input type="text" name="author" require="require" placeholder="Author" class="border border-solid border-blue-600 bg-yellow-50 rounded-md placeholder:text-blue-400 placeholder:text-sm placeholder:font-bold placeholder:font-sans p-2 w-full">
+          <input type="text" name="ISBN" require="require" placeholder="ISBN" class="border border-solid border-blue-600 bg-yellow-50 rounded-md placeholder:text-blue-400 placeholder:text-sm placeholder:font-bold placeholder:font-sans p-2 w-full">
+          <input type="text" name="book_image"  require="require" placeholder="Image URL"  class="border border-solid border-blue-600 bg-yellow-50 rounded-md placeholder:text-blue-400 placeholder:text-sm placeholder:font-bold placeholder:font-sans p-2 w-full">
           <textarea rows="4" cols="1" type="text" name="description" require="require" placeholder="Description" class="border border-solid border-blue-600 bg-yellow-50 rounded-md placeholder:text-blue-400 placeholder:text-sm placeholder:font-bold placeholder:font-sans p-2 w-full"></textarea> 
       </div>
 
